a734627616c3b58212fc9c967aab92daa4bb1da6
Patricia Segantine <pnsegantine@gmail.com>
2025-10-30 22:11:02 +0000

    feat(uploads): implement image upload function and update route logic with validation

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,29 +1,38 @@
-import {FastifyPluginAsyncZod} from "fastify-type-provider-zod";
-import {FastifyInstance} from "fastify";
-import {z} from "zod";
+import {uploadImage} from '@/app/functions/upload-image'
+import type {FastifyPluginAsyncZod} from 'fastify-type-provider-zod'
+import {z} from 'zod'
 
-export const uploadImageRoute: FastifyPluginAsyncZod = async (server: FastifyInstance) => {
-    server.post('/uploads', {
+export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+    server.post(
+        '/uploads',
+        {
             schema: {
                 summary: 'Upload an image',
                 consumes: ['multipart/form-data'],
                 response: {
                     201: z.object({ uploadId: z.string() }),
-                    409: z
-                        .object({ message: z.string() })
-                        .describe('Upload already exists.'),
+                    400: z.object({ message: z.string() }),
                 },
             },
         },
         async (request, reply) => {
-           const uploadedFile = await request.file({
-               limits: {
-                   fileSize: 1024 * 1024 * 2 // 2mb
-               },
-           })
-            console.log('uploadedFile', uploadedFile)
+            const uploadedFile = await request.file({
+                limits: {
+                    fieldSize: 1024 * 1024 * 2, // 2mb
+                },
+            })
 
-        return reply.status(201).send({ uploadId: 'test' })
+            if (!uploadedFile) {
+                return reply.status(400).send({ message: 'File is required.' })
+            }
+
+            await uploadImage({
+                fileName: uploadedFile.filename,
+                contentType: uploadedFile.mimetype,
+                contentStream: uploadedFile.file,
+            })
+
+            return reply.status(201).send({ uploadId: '123' })
         }
     )
 }
