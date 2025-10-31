c95fd422ba769824d7478e4166dcf663eac2d11a
Patricia Segantine <pnsegantine@gmail.com>
2025-10-31 15:10:43 +0000

    feat(storage): implement file upload to Cloudflare R2 with validation and integration
    - Added `uploadFileToStorage` function to handle file uploads with validation.
    - Integrated the function into image upload logic, replacing placeholder TODO.
    - Updated database insertion to store file remote key and URL.

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -2,6 +2,7 @@ import {Readable} from 'node:stream'
 import {db} from '@/infra/db'
 import {schema} from '@/infra/db/schemas'
 import {type Either, makeLeft, makeRight} from '@/infra/shared/either'
+import {uploadFileToStorage} from '@/infra/storage/upload-file-to-storage'
 import {z} from 'zod'
 import {InvalidFileFormat} from './errors/invalid-file-format'
 
@@ -15,22 +16,27 @@ type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput): Promise<Either<InvalidFileFormat, {
-    url: string }>> {
-
+export async function uploadImage(
+    input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
     const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
     if (!allowedMimeTypes.includes(contentType)) {
         return makeLeft(new InvalidFileFormat())
     }
 
-    // TODO: carregar a imagem p/ o Cloudflare R2
+    const { key, url } = await uploadFileToStorage({
+        folder: 'images',
+        fileName,
+        contentType,
+        contentStream,
+    })
 
     await db.insert(schema.uploads).values({
         name: fileName,
-        remoteKey: fileName,
-        remoteUrl: fileName,
+        remoteKey: key,
+        remoteUrl: url,
     })
 
-    return makeRight({ url: '' })
+    return makeRight({ url })
 }
